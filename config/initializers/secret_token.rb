226a0672a31bc74020b64267a0b3590d0730092b
Fernando Araújo <papucho86@gmail.com>
2013-03-07 11:29:28 +0000

    adds rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GbAcademy::Application.config.secret_token = 'd71d6c1ae67b4a58f1130e1750552ac974efce09c87d4f3446b7dcc56d940b65e36fa29a03404d7fc30f7aff000524faa7f1cd6aa2d1ef6eaeb3ab6a9ccf015c'
